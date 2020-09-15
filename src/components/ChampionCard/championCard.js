057cd8ae84e16ca3faca63b50a0ea268f82ef0f0
Kevin Choi <kevin1choi@gmail.com>
2020-09-15 16:44:10 -0700

    Champion card info onClick

--- src/components/ChampionCard/championCard.js
+++ src/components/ChampionCard/championCard.js
@@ -1,12 +1,45 @@
 
 import React from 'react';
 
+const bgStyle = {
+    backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.725) 0%,rgba(0,0,0,0.725) 100%), url('${props.champion.src}')`,
+    backgroundSize: '88%', 
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    height: '450'
+};
+
 function ChampionCard(props) {
-    // add onHover functions
+    const cardstate = {
+        isClicked: true
+    };
+
+    function championInfo() {
+        if (cardstate.isClicked) {
+            cardstate.isClicked === false;
+        } else {
+            cardstate.isClicked === true;
+        }
+    }
 
     return (
         <div className="col-4">
-            <img src={props.champion.src} style={{ height: 400 }}/>
+            {cardstate.isClicked
+                ? <img src={props.champion.src} onClick={championInfo} style={{ height: 450 }}/>
+                : <div className="col-4">
+                    <div style={ bgStyle }>
+                        <div style={{ height: 450, color:"darkgoldenrod" }}>
+                            <br/><br/>
+                            <h5>{ `${props.champion.traits[0]} ${props.champion.traits[1]}` }</h5>
+                            <br/><br/><br/>
+                            <p className="mx-sm-5">{ props.champion.description }</p>
+                            <br/><br/>
+                            <p>{ `Cost: ${props.champion.cost}g` }</p>
+                        </div>
+                    </div>
+                    <h5 className="" style={{ color:"darkgoldenrod" }}>{props.champion.name}</h5>
+                </div>
+            }
             <h5 className="" style={{ color:"darkgoldenrod" }}>{props.champion.name}</h5>
         </div>
    );
