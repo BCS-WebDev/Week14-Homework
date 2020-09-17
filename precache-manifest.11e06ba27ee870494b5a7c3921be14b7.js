402450236b618fff72c1b521f4e0d6dc90e19868
Kevin Choi <kevin1choi@gmail.com>
2020-09-17 15:01:33 -0700

    Updates

---
+++ precache-manifest.11e06ba27ee870494b5a7c3921be14b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335e7f9f8dd61a8756b7d8609de8667f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "112975bfb1a2ae42d2b3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5312abec7a78671c46d0",
+    "url": "/static/js/2.213bffe1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.213bffe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112975bfb1a2ae42d2b3",
+    "url": "/static/js/main.562e02b1.chunk.js"
+  },
+  {
+    "revision": "76c0879cc130edce90a2",
+    "url": "/static/js/runtime-main.2cea7679.js"
+  },
+  {
+    "revision": "32b4bfe18d87be5474541b0c923f4b39",
+    "url": "/static/media/38.32b4bfe1.png"
+  },
+  {
+    "revision": "f9c432472497c7880b68c56ccc82546b",
+    "url": "/static/media/SmallerBoards.f9c43247.png"
+  },
+  {
+    "revision": "30d6440597602ed1793e2632d21122af",
+    "url": "/static/media/none.30d64405.png"
+  },
+  {
+    "revision": "0859d54d0f579aa30323d4953455eaf7",
+    "url": "/static/media/tft-bg.0859d54d.jpg"
+  },
+  {
+    "revision": "a10940f69c6e4c2b3c95dac56a1959fe",
+    "url": "/static/media/tft-logo.a10940f6.png"
+  }
+]);
