496a90e692f59844eed7f59e9ff3ec598b4bfe8b
Kevin Choi <kevin1choi@gmail.com>
2020-09-17 15:07:24 -0700

    Updates

---
+++ precache-manifest.bb0beca162eea3dd4565ff4dd8dfdb5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425abf92fd36d550e935d26a1539b23f",
+    "url": "/BCS-WebDev/Week14-Homework/index.html"
+  },
+  {
+    "revision": "8d3b4a33f6d6bf4295a5",
+    "url": "/BCS-WebDev/Week14-Homework/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5312abec7a78671c46d0",
+    "url": "/BCS-WebDev/Week14-Homework/static/js/2.213bffe1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BCS-WebDev/Week14-Homework/static/js/2.213bffe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3b4a33f6d6bf4295a5",
+    "url": "/BCS-WebDev/Week14-Homework/static/js/main.d7a7ae57.chunk.js"
+  },
+  {
+    "revision": "65cf76bab3298435550b",
+    "url": "/BCS-WebDev/Week14-Homework/static/js/runtime-main.c454b3d6.js"
+  },
+  {
+    "revision": "32b4bfe18d87be5474541b0c923f4b39",
+    "url": "/BCS-WebDev/Week14-Homework/static/media/38.32b4bfe1.png"
+  },
+  {
+    "revision": "f9c432472497c7880b68c56ccc82546b",
+    "url": "/BCS-WebDev/Week14-Homework/static/media/SmallerBoards.f9c43247.png"
+  },
+  {
+    "revision": "30d6440597602ed1793e2632d21122af",
+    "url": "/BCS-WebDev/Week14-Homework/static/media/none.30d64405.png"
+  },
+  {
+    "revision": "0859d54d0f579aa30323d4953455eaf7",
+    "url": "/BCS-WebDev/Week14-Homework/static/media/tft-bg.0859d54d.jpg"
+  },
+  {
+    "revision": "a10940f69c6e4c2b3c95dac56a1959fe",
+    "url": "/BCS-WebDev/Week14-Homework/static/media/tft-logo.a10940f6.png"
+  }
+]);
